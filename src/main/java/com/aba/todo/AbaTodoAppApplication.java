ab4b73343defa09375b15b43f4916498e835d3d5
abdeljabar <aitbelkhalil@gmail.com>
2023-05-17 23:55:57 +0200

    first commit

---
+++ src/main/java/com/aba/todo/AbaTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.aba.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbaTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbaTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aba/todo/AbaTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aba.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbaTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
